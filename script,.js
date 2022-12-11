e3781272aa47c76cfc58c24427e607db82b40111
taras <tarik2002sta@gmail/com>
2022-12-11 19:07:59 +0200

    firct commit

---
+++ script,.js
@@ -0,0 +1,8 @@
+
+
+const firstNumber = +prompt('Введіть перше число');
+const secondNumber = +prompt('Введіть друге число');
+const thirdNumber = +prompt('Введіть третє число');
+
+const sum =  (firstNumber + secondNumber + thirdNumber) / 3;
+alert(sum);
